83ee0c424c85ee152967775eff40f91a1568adfa
Thomas J. Leeper <thosjleeper@gmail.com>
2013-12-05 13:38:57 +0100

    added setUNF and compareUNF functions and docs

--- R/setUNF.r
+++ R/setUNF.r
@@ -1,4 +1,19 @@
-setUNF <- function(x, ver=5){
-    attr(x, 'UNF') <- unf(x, ver=ver)
+setUNF <- function(x, ...){
+    attr(x, 'UNF') <- unf(x, ...)
     return(x)
+}
+
+checkUNF <- function(x, ...){
+    unf <- unf(x, ...)
+    if(!is.null(attr(x, 'UNF'))){
+        old <- attr(x, 'UNF')
+        if(identical(old,unf))
+            message('UNFs are identical\n')
+        else
+            message('UNFs do not match\n')
+    } else{
+        old <- NULL
+        message('No UNF attribute found in `x`\n')
+    }
+    return(list(Attribute = old, Current = unf))
 }
