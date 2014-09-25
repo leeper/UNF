ad01649ccf36a9130074278f5db008ce4d9eda98
Thomas J. Leeper <thosjleeper@gmail.com>
2014-09-25 18:07:56 +0200

    modify nonfinite_as_missing

--- R/nonfinite.r
+++ R/nonfinite.r
@@ -1,6 +1,6 @@
 .nonfinite <- function(x, char, nonfinites_as_missing=FALSE){
     if(nonfinites_as_missing){
-        char <- ifelse(is.nan(x), NA, char)
+        char <- ifelse(!is.finite(x), NA, char)
     } else {
         char <- ifelse((!is.finite(x) & !is.character(x)), paste(tolower(as.character(x)),'\n',sep=''), char)
         char <- ifelse(char=='inf\n', '+inf\n', char)
--- tests/testthat/test-unf6-nonfinite.R
+++ tests/testthat/test-unf6-nonfinite.R
@@ -5,7 +5,7 @@ test_that("Nonfinites handled per specification", {
     expect_equal(unf6("+nan"), unf6(NaN))
 })
 test_that("Nonfinites optionally treated as missing", {
-    #expect_equal(unf6(NA), unf6(Inf, nonfinites_as_missing=TRUE))
-    #expect_equal(unf6(NA), unf6(-Inf, nonfinites_as_missing=TRUE))
-    #expect_equal(unf6(NA), unf6(NaN, nonfinites_as_missing=TRUE))
+    expect_equal(unf6(NA), unf6(Inf, nonfinites_as_missing=TRUE))
+    expect_equal(unf6(NA), unf6(-Inf, nonfinites_as_missing=TRUE))
+    expect_equal(unf6(NA), unf6(NaN, nonfinites_as_missing=TRUE))
 })
