45f7b810a790310ac7e0d110723d3d73e5861008
Thomas J. Leeper <thosjleeper@gmail.com>
2016-06-16 10:23:58 +0100

    test print method and dispatch (#17)

--- tests/testthat/test-unf6-unf-class.R
+++ tests/testthat/test-unf6-unf-class.R
@@ -2,6 +2,11 @@ context("UNFv6: UNF Class Object")
 test_that("Object is 'UNF' class", {
     expect_equal(class(unf6(1)), "UNF")
 })
+
+test_that("'UNF' class object prints", {
+    expect_equal(print(unf6(1)), "UNF")
+})
+
 test_that("Object slots", {
     expect_equal(names(unf6(1)), c("unf","hash","unflong","formatted"), label = "object names correct")
     expect_equal(class(unf6(1)$unf), "character", label = "unf is character")
@@ -9,6 +14,7 @@ test_that("Object slots", {
     expect_equal(class(unf6(1)$unflong), "character", label = "unflong is character")
     expect_equal(class(unf6(1)$formatted), "character", label = "formatted is character")
 })
+
 test_that("Attributes", {
     expect_equal(attr(unf6(1), "class"), "UNF", label = "class")
     expect_equal(attr(unf6(1), "version"), 6, label = "version")
